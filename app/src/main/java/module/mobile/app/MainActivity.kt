7554a46eb4363fc8a335de54056a9c8ceae8cdf8
Roman4k-gg <sendelroma@gmail.com>
2026-03-20 18:17:55 +0700

    добавил сетку почти

--- app/src/main/java/module/mobile/app/MainActivity.kt
+++ app/src/main/java/module/mobile/app/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import module.mobile.app.ui.theme.MobilemoduleTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +33,7 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    ScrollingGrid()
                 }
             }
         }
@@ -33,16 +43,29 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "",
         modifier = modifier
     )
 }
 
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MobilemoduleTheme {
-        Greeting("Android")
+fun ScrollingGrid() {
+    val itemsList = (0..24).toList()
+
+    val itemModifier = Modifier
+        .height(20.dp)
+        .width(20.dp)
+        .wrapContentSize()
+
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(5),
+        horizontalArrangement = Arrangement.spacedBy(0.dp),
+        verticalArrangement = Arrangement.spacedBy(0.dp)
+    ) {
+        items(itemsList) {
+            Text("", itemModifier)
+        }
+
     }
 }
